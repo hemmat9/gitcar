f0fa53b894674f935b04afedb756963a8ec426b7
“hemmat9” <farhad.hemmat9@gmail.com>
2022-01-26 17:56:02 -0800

    there are the changes you asked me to make

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         CarBasics car = new CarBasics();
         car.color = "red";
-        car.engine = "7999cc";
+        car.engine = "79yy99cc";
         car.door ="2";
-        car.number= "119018";
+        car.number= "119nn018";
 
         System.out.println(car.color);
 
         car.color ="green";
         car.number ="789";
-        car.engine = "4545454cc";
-        car.door = "3";
+        car.engine = "454cc";
+        car.door = "63";
 
         System.out.println(car.color);
     }
